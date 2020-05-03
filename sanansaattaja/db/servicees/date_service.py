0e23ca389fc12084c57b53e390d2a05950f9fd14
Orijuke <zaitsevgeorge.ru@gmail.com>
2020-05-03 17:03:34 +0500

    Fix posts third time

--- sanansaattaja/db/servicees/date_service.py
+++ sanansaattaja/db/servicees/date_service.py
@@ -2,6 +2,6 @@ def get_date(datetime):
     months = ['January', 'February', 'March', 'April',
               'May', 'June', 'July', 'August', 'September',
               'October', 'November', 'December']
-    date = months[datetime.month] + ' ' + str(datetime.day) + ', ' + str(datetime.year)
+    date = months[datetime.month - 1] + ' ' + str(datetime.day) + ', ' + str(datetime.year)
     time = ' at ' + str(datetime.hour) + ':' + str(datetime.minute)
     return date + time
