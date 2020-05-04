f1d717b04a193b8aac26d40ae86d431e15e9f19f
KEZKA <world.is.unpredictable@gmail.com>
2020-05-04 14:47:20 +0500

    adds method get_normal_date

--- sanansaattaja/db/data/models/post.py
+++ sanansaattaja/db/data/models/post.py
@@ -2,6 +2,7 @@ import datetime
 
 from sqlalchemy import Column, Integer, DateTime, ForeignKey, orm, Boolean, Text, String
 
+from sanansaattaja.core.utils import get_date
 from sanansaattaja.db.data.db_session import SqlAlchemyBase
 
 
@@ -18,3 +19,6 @@ class Post(SqlAlchemyBase):
 
     def __repr__(self):
         return f'<Post> {self.id} {self.author_id}'
+
+    def get_normal_date(self):
+        return get_date(self.modified_date)
--- sanansaattaja/db/servicees/date_service.py
+++
@@ -1,7 +0,0 @@
-def get_date(datetime):
-    months = ['January', 'February', 'March', 'April',
-              'May', 'June', 'July', 'August', 'September',
-              'October', 'November', 'December']
-    date = months[datetime.month - 1] + ' ' + str(datetime.day) + ', ' + str(datetime.year)
-    time = ' at ' + str(datetime.hour) + ':' + str(datetime.minute)
-    return date + time
